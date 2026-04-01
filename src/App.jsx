bf0fcc9ceddc36c0f4dd7ef4991364b01bf00100
Sahil Bhutto <sahil.bhutto.dev@gmail.com>
2026-04-01 21:58:24 +0500

    Initial Code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center font-semibold text-4xl text-purple-700 mt-20">Google Gemini Clone</h1>
+    </div>
+  )
+}
+
+export default App
